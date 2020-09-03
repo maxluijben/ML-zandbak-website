0caef16cf4b61e7d61c7e1aaa1d0579d4d142bec
Maxie35 <max@gopublic.nl>
2020-09-03 21:01:53 +0200

    removed mysql password

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'ml_zandbak_website' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
